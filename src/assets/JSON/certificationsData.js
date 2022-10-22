ce1700cd8db3b80ccc7e1ba54f19e239a6a6a39c
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-10-22 17:40:41 +0100

    carousel - selected certifications

---
+++ src/assets/JSON/certificationsData.js
@@ -0,0 +1,49 @@
+import AZ305 from "../images/certifications/AZ305.png";
+// import AZ305Exam from "../images/certifications/AZ305Exam.png";
+
+import AZ204 from "../images/certifications/AZ204.png";
+import AZ104 from "../images/certifications/AZ104.png";
+
+import AI900 from "../images/certifications/AI900.png";
+import AZ900 from "../images/certifications/AI900.png";
+import SC900 from "../images/certifications/SC900.png";
+import DP900 from "../images/certifications/DP900.png";
+
+import AWSPractitioner from "../images/certifications/AWSPractitioner.png";
+
+const certificationData = [
+  {
+    title: "AZ-305",
+    img: AZ305,
+  },
+  {
+    title: "AZ-204",
+    img: AZ204,
+  },
+  {
+    title: "AZ-104",
+    img: AZ104,
+  },
+  {
+    title: "AI-900",
+    img: AI900,
+  },
+  {
+    title: "AZ-900",
+    img: AZ900,
+  },
+  {
+    title: "SC-900",
+    img: SC900,
+  },
+  {
+    title: "DP-900",
+    img: DP900,
+  },
+  {
+    title: "AWS Practitioner",
+    img: AWSPractitioner,
+  },
+];
+
+export { certificationData };

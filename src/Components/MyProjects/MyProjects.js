9c6b7618457e3c2d96df65e3a7381046b16c1f32
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-10-24 19:43:53 +0100

    separated ui component goldbutton

--- src/Components/MyProjects/MyProjects.js
+++ src/Components/MyProjects/MyProjects.js
@@ -1,6 +1,8 @@
 import React from "react";
 import classes from "./MyProjects.module.scss";
 
+// Icon by <a href="https://freeicons.io/profile/714">Raj Dev</a> on <a href="https://freeicons.io">freeicons.io</a>
+import linkIcon from "../../assets/icons/linkIcon.svg";
 import githubLogo from "../../assets/icons/iconmonstr-github-2.svg";
 
 // TODO: Think about a nice way of displaying here project in next version
@@ -20,6 +22,14 @@ const MyProjects = (props) => {
         target="_blank"
       >
         <img src={githubLogo} alt="Git Hub logo" />
+      </a>
+      <a
+        rel="noreferrer"
+        className={classes.links}
+        href="https://github.com/Pat-On"
+        target="_blank"
+      >
+        <img src={linkIcon} alt="By Raj Dev https://freeicons.io/profile/714" />
       </a>
     </div>
   );

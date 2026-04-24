437032e5fe7e1ca814640d4371256a502cffc628
Patryk <patryk.r.nowak@gmail.com>
2026-04-24 14:14:12 +0100

    improvements to the mobile version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.scss";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Scene from "./TreeJSComponents/Scene";
 import Layout from "./HOC/Layout";
 import ExploreButton from "./Components/ExploreButton/ExploreButton";
@@ -10,12 +10,48 @@ function App() {
   const [exploring, setExploring] = useState(false);
   const isMobile = isMobileDevice();
 
+  const toggleExplore = () => {
+    const entering = !exploring;
+    setExploring(entering);
+    if (isMobile) {
+      if (entering) {
+        const el = document.documentElement;
+        (el.requestFullscreen?.() ?? el.webkitRequestFullscreen?.())?.catch?.(() => {});
+      } else {
+        if (document.fullscreenElement || document.webkitFullscreenElement) {
+          document.exitFullscreen?.() ?? document.webkitExitFullscreen?.();
+        }
+      }
+    }
+  };
+
+  const handleExploreEnd = () => {
+    setExploring(false);
+    if (document.fullscreenElement || document.webkitFullscreenElement) {
+      document.exitFullscreen?.() ?? document.webkitExitFullscreen?.();
+    }
+  };
+
+  useEffect(() => {
+    const onFSChange = () => {
+      if (!document.fullscreenElement && !document.webkitFullscreenElement) {
+        setExploring(false);
+      }
+    };
+    document.addEventListener("fullscreenchange", onFSChange);
+    document.addEventListener("webkitfullscreenchange", onFSChange);
+    return () => {
+      document.removeEventListener("fullscreenchange", onFSChange);
+      document.removeEventListener("webkitfullscreenchange", onFSChange);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <Scene exploring={exploring} onExploreEnd={() => setExploring(false)} />
+      <Scene exploring={exploring} onExploreEnd={handleExploreEnd} />
       <ExploreButton
         exploring={exploring}
-        onToggle={() => setExploring((e) => !e)}
+        onToggle={toggleExplore}
         isMobile={isMobile}
       />
       {exploring && <MobileExploreOverlay key={exploring} />}

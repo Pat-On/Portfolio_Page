82d843caf36ea36f4610109ca014510143fa2e53
Patryk <patryk.r.nowak@gmail.com>
2026-06-14 09:18:17 +0100

    issue to error on iphone

--- src/TreeJSComponents/spaceTexture/spaceTexture.js
+++ src/TreeJSComponents/spaceTexture/spaceTexture.js
@@ -1,8 +1,20 @@
 import * as THREE from "three";
 import space8k from "../../textures/starmap_8k.webp";
+import space4k from "../../textures/starmap_4k.webp";
+import space2k from "../../textures/starmap_2k.webp";
+import { isMobileDevice } from "../../utils/isMobileDevice";
 
 
-const spaceTexture = new THREE.TextureLoader().load(space8k);
+function pickStarmap() {
+  if (isMobileDevice()) {
+    // High-DPI phones can handle 4K (~33 MB); keep low-DPI / older devices at
+    // 2K (~8 MB) where the memory ceiling is lowest.
+    return window.devicePixelRatio > 2 ? space4k : space2k;
+  }
+  return space8k;
+}
+
+const spaceTexture = new THREE.TextureLoader().load(pickStarmap());
 spaceTexture.mapping = THREE.EquirectangularReflectionMapping;
 
 export { spaceTexture };
